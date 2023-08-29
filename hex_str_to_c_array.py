00bba4d1555da8b9c1dcef44edaf715e892da3e6
zjchen666 <33012472+zjchen666@users.noreply.github.com>
2023-08-29 10:48:19 -0700

    Update hex_str_to_c_array.py

--- hex_str_to_c_array.py
+++ hex_str_to_c_array.py
@@ -7,7 +7,7 @@ def hex_to_c_array(hex_string):
     c_array = "{\n"
     for i in range(0, len(byte_array), 8):
         chunk = byte_array[i:i+8]
-        c_array += "    " + ", ".join(["0x{:02X}".format(byte) for byte in chunk]) + ",\n"
+        c_array += "    " + ", ".join(["0x{:02x}".format(byte) for byte in chunk]) + ",\n"
     c_array += "};"
     return c_array
 
