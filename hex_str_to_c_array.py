2a0a0ca46b4a70fd87b18af1599b3bdb0dec9d43
zjchen666 <33012472+zjchen666@users.noreply.github.com>
2023-08-28 18:04:09 -0700

    Create hex_str_to_c_array.py

---
+++ hex_str_to_c_array.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+// command: python3 hex_str_to_c_array.py string
+import sys
+
+def hex_to_c_array(hex_string):
+    byte_array = bytes.fromhex(hex_string)
+    c_array = "{\n"
+    for i in range(0, len(byte_array), 8):
+        chunk = byte_array[i:i+8]
+        c_array += "    " + ", ".join(["0x{:02X}".format(byte) for byte in chunk]) + ",\n"
+    c_array += "};"
+    return c_array
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python hex_to_c_array.py <hexadecimal_string>")
+        sys.exit(1)
+    
+    hex_string = sys.argv[1]
+    c_array = hex_to_c_array(hex_string)
+    print("C Array Format:")
+    print(c_array)
